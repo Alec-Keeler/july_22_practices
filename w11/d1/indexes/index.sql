e2ef6e06625f695aa0620369dba9313b05294c2d
Alec Keeler <alec.keeler@gmail.com>
2022-09-12 15:04:32 -0500

    indexes scenario exercise

---
+++ w11/d1/indexes/index.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(200) NOT NULL,
+    content TEXT NOT NULL
+);
+
+-- 10 users sign up for our website every day, and we get about 5 update/delete requests per day.
+-- Our website only allows admins to search for users, which only happens a few times a day
+
+-- 10 posts are created on our website every day, and we get about 15 update/delete requests per day
+-- Our website has a search feature available to all users to find posts related to the given search term, this is one of the app's primary features.
+
+
+-- Create an index for finding posts by title
+-- Good or bad?
+-- Code:
+
+-- Create an index for finding users by email and username
+-- Good or bad?
+-- Code:
