abd5392e3ee033360ab93388edaf01b8bb20e2c7
Alec Keeler <alec.keeler@gmail.com>
2022-09-12 15:09:17 -0500

    edits

--- w11/d1/indexes/index.sql
+++ w11/d1/indexes/index.sql
@@ -12,16 +12,16 @@ CREATE TABLE posts (
 );
 
 -- 10 users sign up for our website every day, and we get about 5 update/delete requests per day.
--- Our website only allows admins to search for users, which only happens a few times a day
+-- Our website only allows admins to search for users, which only happens a few times a day, but looks up users by email and username.
 
 -- 10 posts are created on our website every day, and we get about 15 update/delete requests per day
--- Our website has a search feature available to all users to find posts related to the given search term, this is one of the app's primary features.
+-- Our website has a search feature available to all users to find posts with titles related to the given search term, this is one of the app's primary features.
 
 
--- Create an index for finding posts by title
+-- Create an index on posts
 -- Good or bad?
 -- Code:
 
--- Create an index for finding users by email and username
+-- Create an index on users
 -- Good or bad?
 -- Code:
